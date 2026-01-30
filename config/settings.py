029f833a05e2e59c3a04b9dbe7e2ac2f29319614
JulianOrtiz972 <ortizjaimesandresjulian120@gmail.com>
2026-01-30 15:03:24 -0500

    settings app

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,18 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "api",
+    "api",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
